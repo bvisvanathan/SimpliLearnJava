d105bd695a25c15cfd18c1abc90d9ed60c3248ae
bvisvanathan <balaji.visvanathan@gmail.com>
2020-10-03 15:51:35 +0000

    commiting inheritance collections 20200310

---
+++ JavaEssentials/src/collections/QueueExample.java
@@ -0,0 +1,44 @@
+package collections;
+
+
+import java.util.Iterator;
+
+// Java program to demonstrate a Queue 
+
+import java.util.PriorityQueue; 
+
+public class QueueExample {
+
+
+	public static void main(String args[]) 
+    { 
+        // Creating empty priority queue 
+        PriorityQueue<String> cities = new PriorityQueue<String>(); 
+  
+        // Adding items to the pQueue using add() 
+        cities.add("Mysuru"); 
+        cities.add("Toronto"); 
+        cities.add("Dublin"); 
+        cities.add("Singapore");
+        cities.add("Nairobi");
+        
+        /*CHALLENGE - Use a Comparator to control the type of sorting that happens in the queue */
+  
+        // Printing the top element of PriorityQueue 
+        System.out.println("Top of the list: "+cities.peek()); 
+  
+        //loop thru all elements
+        System.out.println("Complete list:");
+        Iterator <String>  itr = cities.iterator();
+        while(itr.hasNext())
+        {
+        	System.out.println(itr.next());
+        }
+        // Printing the top element and removing it 
+        // from the PriorityQueue container 
+        System.out.println("Removing this top item: "+cities.poll()); 
+  
+        // Printing the top element again 
+        System.out.println("Look at the top again: " +cities.peek()); 
+    } 
+} 
